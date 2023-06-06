9929c1e831bb10cd8c940fc7df8861d8b44f16b3
hassaan43 <hassaansabir43@gmail.com>
2023-06-06 11:19:53 -0400

    gg

--- new/src/Mover.java
+++ new/src/Mover.java
@@ -2,4 +2,25 @@
 
 public class Mover {
     
+    boolean[][] state;
+    int gridSize;
+    int max;
+    int increment;
+    
+    public Mover() {
+        gridSize = 20;
+        increment = 4;
+        max = 400;
+        state = new boolean[20][20];
+        for (int i = 0; i < 20; i++) {
+          for (int j = 0; j < 20; j++) {
+            state[i][j] = false;
+          }
+        }
+    }
+
+    
+    public boolean isValidDest(int x, int y) {
+        return true;
+    }
 }
