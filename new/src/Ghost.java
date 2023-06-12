9c83695cbe9454fe9b4966e479e415ddafb674df
hassaan43 <hassaansabir43@gmail.com>
2023-06-12 01:10:07 -0400

    some changes

--- new/src/Ghost.java
+++ new/src/Ghost.java
@@ -1,58 +1,192 @@
+import java.awt.*;
 
+public class Ghost {
 
-public class Ghost extends Mover {
+  private int ghostDirection; // 0 = Right, 1 = Down, 2 = Left, 3 = Up
+  private int ghostX; // x position
+  private int ghostY; // y position
+  private Image ghostLeft;
+  private Image ghostRight;
 
-    char direction; //U = up, D = down, L = Left, R = Right
+  public Ghost(Image leftImage, Image rightImage, int x, int y) {
+    this.ghostX = x;
+    this.ghostY = y - 20;
+    this.ghostLeft = leftImage;
+    this.ghostRight = rightImage;
+    this.ghostDirection = 3;
+  }
 
-    int x; //x position
-    int y; //y position
+  public void teleportGhost() {
+    if (this.ghostX == 0 * Board.GRID_SIZE && this.ghostY == 14 * Board.GRID_SIZE) {
+      this.ghostX = 26 * Board.GRID_SIZE;
+    }
+    if (this.ghostX == 27 * Board.GRID_SIZE && this.ghostY == 14 * Board.GRID_SIZE) {
+      this.ghostX = 1 * Board.GRID_SIZE;
+    }
+  }
 
-    int lastX; //old x position
-    int lastY; //old y position
-    
-    public Ghost(int x, int y) {
+  public void validGhostMove() {
+    // Calculate the next position based on the current direction
+    int nextX = this.ghostX;
+    int nextY = this.ghostY;
 
+    switch (this.ghostDirection) {
+      case 0: // Right
+        nextX += Board.GRID_SIZE;
+        break;
+      case 1: // Down
+        nextY += Board.GRID_SIZE;
+        break;
+      case 2: // Left
+        nextX -= Board.GRID_SIZE;
+        break;
+      case 3: // Up
+        nextY -= Board.GRID_SIZE;
+        break;
     }
 
-    public void move() {
-        lastX = x;
-        lastY = y;
-    
-        switch (direction) {
-        case 'L':
-          if (isValidDest(x - increment, y)) x -= increment;
-          break;
-        case 'R':
-          if (isValidDest(x + gridSize, y)) x += increment;
-          break;
-        case 'U':
-          if (isValidDest(x, y - increment)) y -= increment;
-          break;
-        case 'D':
-          if (isValidDest(x, y + gridSize)) y += increment;
-          break;
+    // Check if the next position is valid (not out of bounds and not a wall or a
+    // ghost gate)
+    if (nextX >= 0 && nextX < Board.WIDTH && nextY >= 0 && nextY < Board.HEIGHT &&
+        (Board.mapObjects[nextY / Board.GRID_SIZE][nextX / Board.GRID_SIZE] != 1)
+        && (Board.mapObjects[nextY / Board.GRID_SIZE][nextX / Board.GRID_SIZE] != 3)) {
+      this.ghostX = nextX;
+      this.ghostY = nextY;
+    }
+  }
+
+  private int noBackwardGhostDirection() {
+    int newDirection = 0;
+
+    switch (this.ghostDirection) {
+      case 0: // Right
+        newDirection = (int) (Math.random() * 3);
+        switch (newDirection) {
+          case 0:
+            newDirection = 0;
+            break;
+          case 1:
+            newDirection = 1;
+            break;
+          case 2:
+            newDirection = 3;
+            break;
         }
-    }
-
-    private char newDirection() {
-        return 0;
-    }
-
-    public void updatePellet() {
-        int tempX, tempY;
-        int lastPelletX, lastPelletY;
-        int pelletX, pelletY;
-
-        tempX = x / gridSize - 1;
-        tempY = y / gridSize - 1;
-        if (tempX != pelletX || tempY != pelletY) {
-          lastPelletX = pelletX;
-          lastPelletY = pelletY;
-          pelletX = tempX;
-          pelletY = tempY;
+        break;
+      case 1: // Down
+        newDirection = (int) (Math.random() * 3);
+        switch (newDirection) {
+          case 0:
+            newDirection = 0;
+            break;
+          case 1:
+            newDirection = 1;
+            break;
+          case 2:
+            newDirection = 2;
+            break;
         }
-    
-      }
+        break;
+      case 2: // Left
+        newDirection = (int) (Math.random() * 3);
+        switch (newDirection) {
+          case 0:
+            newDirection = 1;
+            break;
+          case 1:
+            newDirection = 2;
+            break;
+          case 2:
+            newDirection = 3;
+            break;
+        }
+        break;
+      case 3: // Up
+        newDirection = (int) (Math.random() * 3);
+        switch (newDirection) {
+          case 0:
+            newDirection = 0;
+            break;
+          case 1:
+            newDirection = 2;
+            break;
+          case 2:
+            newDirection = 3;
+            break;
+        }
+        break;
+    }
+    return newDirection;
+  }
 
+  public void changeGhostDirection(int newDirection) {
+    int tempX = this.ghostX;
+    int tempY = this.ghostY;
 
+    switch (newDirection) {
+      case 0: // Right
+        tempX += Board.GRID_SIZE;
+        break;
+      case 1: // Down
+        tempY += Board.GRID_SIZE;
+        break;
+      case 2: // Left
+        tempX -= Board.GRID_SIZE;
+        break;
+      case 3: // Up
+        tempY -= Board.GRID_SIZE;
+        break;
+    }
+
+    if (tempX >= 0 && tempX < Board.WIDTH && tempY >= 0 && tempX < Board.HEIGHT &&
+        Board.mapObjects[tempY / Board.GRID_SIZE][tempX / Board.GRID_SIZE] != 1) {
+      this.ghostDirection = newDirection;
+    }
+  }
+
+  int count = 0;
+
+  public void ghostMove() {
+    if (count < 4) {
+      ghostStart();
+      count++;
+    } else {
+      changeGhostDirection(noBackwardGhostDirection());
+      validGhostMove();
+      teleportGhost();
+    }
+  }
+
+  private void ghostStart() {
+    int nextX = this.ghostX;
+    int nextY = this.ghostY;
+
+    nextY -= Board.GRID_SIZE;
+
+    if (nextX >= 0 && nextX < Board.WIDTH && nextY >= 0 && nextY < Board.HEIGHT &&
+        (Board.mapObjects[nextY / Board.GRID_SIZE][nextX / Board.GRID_SIZE] != 1)) {
+      this.ghostX = nextX;
+      this.ghostY = nextY;
+    }
+  }
+
+  public int getGhostX() {
+    return this.ghostX;
+  }
+
+  public int getGhostY() {
+    return this.ghostY;
+  }
+
+  public int getGhostDirection() {
+    return this.ghostDirection;
+  }
+
+  public Image getGhostLeft() {
+    return this.ghostLeft;
+  }
+
+  public Image getGhostRight() {
+    return this.ghostRight;
+  }
 }
--- new/src/Pacman.java
+++ new/src/Pacman.java
@@ -1,48 +1,102 @@
-import java.util.*;
+import java.awt.*;
 
-public class Pacman extends Mover {
+public class Pacman {
 
-    char direction; //U = up, D = down, L = Left, R = Right
+    static int pacmanDirection = 4; // 0 = Right, 1 = Down, 2 = Left, 3 = Up, 4 = Start
+    static int pacmanX = 260; // x position
+    static int pacmanY = 460; // y position
+    static int prevX;
+    static int prevY;
+    static Image pacmanLeft;
+    static Image pacmanRight;
+    static Image pacmanUp;
+    static Image pacmanDown;
+    static Image pacman;
 
-    int x; //x position
-    int y; //y position
-
-    int lastX; //old x position
-    int lastY; //old y position
-
-    public Pacman(int x, int y) {
-
+    public Pacman(Image leftImage, Image rightImage, Image upImage, Image downImage, Image pacmanStill) {
+        pacmanLeft = leftImage;
+        pacmanRight = rightImage;
+        pacmanDown = downImage;
+        pacmanUp = upImage;
+        pacman = pacmanStill;
     }
 
-    public void move() {
-      switch (direction) {
-      //if move left decrease x value
-      case 'L':
-        if (isValidDest(x - increment, y)) x -= increment;
-        break;
-      
-      //if move right increase x value
-      case 'R':
-        if (isValidDest(x + gridSize, y)) x += increment;
-        break;
+    public static void teleportPacman() {
+        if (pacmanX == 0*Board.GRID_SIZE && pacmanY == 14*Board.GRID_SIZE) {
+                pacmanX = 26*Board.GRID_SIZE;
+        }
+        if (pacmanX == 27*Board.GRID_SIZE && pacmanY == 14*Board.GRID_SIZE) {
+                pacmanX = 1*Board.GRID_SIZE;
+        }
+    }
+
+    public static void validPacmanMove() {
+        // Calculate the next position based on the current direction
+        int nextX = pacmanX;
+        int nextY = pacmanY;
+        prevY = pacmanY;
+        prevX = pacmanX;
+
+        switch (pacmanDirection) {
+            case 0: // Right
+                nextX += Board.GRID_SIZE;
+                break;
+            case 1: // Down
+                nextY += Board.GRID_SIZE;
+                break;
+            case 2: // Left
+                nextX -= Board.GRID_SIZE;
+                break;
+            case 3: // Up
+                nextY -= Board.GRID_SIZE;
+                break;
+        }
         
-      //if move up decrease y value
-      case 'U':
-        if (isValidDest(x, y - increment)) y -= increment;
-        break;
-          
-      //if move down increase y value
-      case 'D':
-        if (isValidDest(x, y + gridSize)) y += increment;
-        break;
-      }
+        // Check if the next position is valid (not out of bounds and not a wall or a ghost gate)
+        if (nextX >= 0 && nextX < Board.WIDTH && nextY >= 0 && nextY < Board.HEIGHT &&
+                (Board.mapObjects[nextY / Board.GRID_SIZE][nextX / Board.GRID_SIZE] != 1 && Board.mapObjects[nextY / Board.GRID_SIZE][nextX / Board.GRID_SIZE] != 3)) {
+            pacmanX = nextX;
+            pacmanY = nextY;
+        }
     }
-  }
 
-  public void updatePellet() {
-    if (x % gridSize == 0 && y % gridSize == 0) {
-      pelletX = x / gridSize - 1;
-      pelletY = y / gridSize - 1;
+    public static void pickPellet() {
+        // Check if the current position contains a pellet (represented by value 2 in mapObjects)
+        if (Board.mapObjects[pacmanY / Board.GRID_SIZE][pacmanX / Board.GRID_SIZE] == 2) {
+            Game.score++; // Increase the score
+            Board.mapObjects[pacmanY / Board.GRID_SIZE][pacmanX / Board.GRID_SIZE] = 0; // Remove the pellet from the map
+        
+            // Update the high score if the current score is higher
+            if (Game.score > Board.highScore) {
+                Board.highScore = Game.score;
+            }
+        }
     }
-  }
+
+    public static void changePacmanDirection(int newDirection) {
+        int tempX = pacmanX;
+        int tempY = pacmanY;
+
+        switch (newDirection) {
+            case 0: // Right
+                tempX += Board.GRID_SIZE;
+                break;
+            case 1: // Down
+                tempY += Board.GRID_SIZE;
+                break;
+            case 2: // Left
+                tempX -= Board.GRID_SIZE;
+                break;
+            case 3: // Up
+                tempY -= Board.GRID_SIZE;
+                break;
+        }
+
+        if (tempX >= 0 && tempX < Board.WIDTH && tempY >= 0 && tempX < Board.HEIGHT &&
+              Board.mapObjects[tempY / Board.GRID_SIZE][tempX / Board.GRID_SIZE] != 1) {
+            pacmanDirection = newDirection;
+        }
+    }
+
+
 }
